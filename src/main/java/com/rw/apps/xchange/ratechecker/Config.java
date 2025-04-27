762074b588a7b6519e0461e1885ef8d7bdf6722d
rw00 <raafatwahb@gmail.com>
2025-04-27 12:06:38 +0200

    fix bot listener

---
+++ src/main/java/com/rw/apps/xchange/ratechecker/Config.java
@@ -0,0 +1,19 @@
+package com.rw.apps.xchange.ratechecker;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class Config {
+    @Bean
+    @Profile("!" + Constants.LOCAL_TEST_PROFILE)
+    TelegramBotsApi telegramBotsApi(TelegramBotListener botListener) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(botListener);
+        return botsApi;
+    }
+}
